74ea2a67852f3fc73813d3e6ba789892508197a2
Leonardo <lezera12@gmail.com>
2022-05-02 16:05:12 -0300

    Updates

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -4,6 +4,7 @@ export interface TicketUpdateddEvent {
     subject: Subjects.TicketUpdated;
     data: {
         id: string
+        version: number
         title: string
         price: number
         userdId: string
